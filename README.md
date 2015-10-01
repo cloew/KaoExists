67d0bd9af1c8704957fb55fc2dd27e4652e82f85
Chris Loew <cloew123@gmail.com>
2015-10-01 10:56:50 -0600

    Fixed naming

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# KaoExists
+# KaoTry
 A simple library to provide simple inline existence checks to allow retrieving a possibly child value.
 eg a?.b?.c?.d where a, b, c could be None
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name='kao_exists',
+setup(name='kao_try',
       version='0.0.1',
-      description="",
-      author='',
-      author_email='',
-      packages=['kao_exists'],
+      description="Library to provide simple chaining a la Monads...?",
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      packages=['kao_try'],
      )
